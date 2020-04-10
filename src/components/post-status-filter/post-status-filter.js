dc164b7d99f887594556952dd203b243fe2411f8
Abay <abayibrayev7@gmail.com>
2020-04-10 17:09:14 +0100

    implemented styling for the components

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./post-status-filter.css";
 
 const PostStatusFilter = () => {
   return (
     <div className="btn-group">
-      <button />
-      <button />
+      <button>All</button>
+      <button>Favourite</button>
     </div>
   );
 };
