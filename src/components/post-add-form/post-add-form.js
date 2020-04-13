c31105d4c73c78637ca2bbecf7fa25780e728590
Abay <abayibrayev7@gmail.com>
2020-04-13 17:33:43 +0100

    added posts adding functionality

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import { Button } from "reactstrap";
 import "./post-add-form.css";
 
-const PostAddForm = ({ onAdd }) => {
-  return (
-    <form className="bottom-panel d-flex">
-      <input
-        type="text"
-        placeholder="What are you thinking about?"
-        className="form-control new-post-label"
-      />
-      <Button type="submit" outline onClick={() => onAdd("Hello")}>
-        Add
-      </Button>
-    </form>
-  );
-};
+export default class PostAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+    };
+    this.onValueChange = this.onValueChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-export default PostAddForm;
+  onValueChange(e) {
+    this.setState({
+      text: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    if (this.state.text.length > 0) {
+      this.props.onAdd(this.state.text);
+      this.setState({
+        text: "",
+      });
+    }
+  }
+
+  render() {
+    return (
+      <form className="bottom-panel d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          placeholder="What are you thinking about?"
+          className="form-control new-post-label"
+          onChange={this.onValueChange}
+          value={this.state.text}
+        />
+        <Button type="submit" outline>
+          Add
+        </Button>
+      </form>
+    );
+  }
+}
