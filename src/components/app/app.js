e3c4dc4621d2c7ec4a0cc9de764e48fc8d2be89d
Abay <abayibrayev7@gmail.com>
2020-04-13 17:45:07 +0100

    implemented the search functionality

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -30,11 +30,13 @@ export default class App extends Component {
           id: 3,
         },
       ],
+      term: "",
     };
     this.deleteItem = this.deleteItem.bind(this);
     this.addItem = this.addItem.bind(this);
     this.onToggleImportant = this.onToggleImportant.bind(this);
     this.onToggleLiked = this.onToggleLiked.bind(this);
+    this.onUpdateSearch = this.onUpdateSearch.bind(this);
 
     this.maxId = 4;
   }
@@ -100,19 +102,34 @@ export default class App extends Component {
     });
   }
 
+  searchPost(items, term) {
+    if (term.length === 0) {
+      return items;
+    }
+
+    return items.filter((item) => {
+      return item.label.indexOf(term) > -1;
+    });
+  }
+
+  onUpdateSearch(term) {
+    this.setState({ term });
+  }
+
   render() {
-    const { data } = this.state;
+    const { data, term } = this.state;
     const liked = data.filter((item) => item.like).length;
     const allPosts = data.length;
+    const visiblePosts = this.searchPost(data, term);
     return (
       <div className="app">
         <AppHeader liked={liked} allPosts={allPosts} />
         <div className="search-panel d-flex">
-          <SearchPanel />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
           <PostStatusFilter />
         </div>
         <PostList
-          posts={data}
+          posts={visiblePosts}
           onDelete={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleLiked={this.onToggleLiked}
