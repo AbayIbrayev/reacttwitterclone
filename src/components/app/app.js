1b3f6ae986c2948a6f4c6b70cf2ff0c16e0968aa
Abay <abayibrayev7@gmail.com>
2020-04-13 18:17:42 +0100

    implemented a filter functionality

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -31,12 +31,14 @@ export default class App extends Component {
         },
       ],
       term: "",
+      filter: "all",
     };
     this.deleteItem = this.deleteItem.bind(this);
     this.addItem = this.addItem.bind(this);
     this.onToggleImportant = this.onToggleImportant.bind(this);
     this.onToggleLiked = this.onToggleLiked.bind(this);
     this.onUpdateSearch = this.onUpdateSearch.bind(this);
+    this.onFilterSelect = this.onFilterSelect.bind(this);
 
     this.maxId = 4;
   }
@@ -112,21 +114,36 @@ export default class App extends Component {
     });
   }
 
+  filterPost(items, filter) {
+    if (filter === "like") {
+      return items.filter((items) => items.like);
+    } else {
+      return items;
+    }
+  }
+
   onUpdateSearch(term) {
     this.setState({ term });
   }
 
+  onFilterSelect(filter) {
+    this.setState({ filter });
+  }
+
   render() {
-    const { data, term } = this.state;
+    const { data, term, filter } = this.state;
     const liked = data.filter((item) => item.like).length;
     const allPosts = data.length;
-    const visiblePosts = this.searchPost(data, term);
+    const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
     return (
       <div className="app">
         <AppHeader liked={liked} allPosts={allPosts} />
         <div className="search-panel d-flex">
           <SearchPanel onUpdateSearch={this.onUpdateSearch} />
-          <PostStatusFilter />
+          <PostStatusFilter
+            filter={filter}
+            onFilterSelect={this.onFilterSelect}
+          />
         </div>
         <PostList
           posts={visiblePosts}
--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,18 +1,31 @@
-import React from "react";
-import { Button } from "reactstrap";
+import React, { Component } from "react";
 import "./post-status-filter.css";
 
-const PostStatusFilter = () => {
-  return (
-    <div className="btn-group">
-      <Button color="info" type="button" className="btn btn-info">
-        All
-      </Button>
-      <Button outline type="button">
-        Favourite
-      </Button>
-    </div>
-  );
-};
-
-export default PostStatusFilter;
+export default class PostStatusFilter extends Component {
+  constructor(props) {
+    super(props);
+    this.buttons = [
+      { name: "all", label: "All" },
+      { name: "like", label: "Liked" },
+    ];
+  }
+  render() {
+    const buttons = this.buttons.map(({ name, label }) => {
+      const { filter, onFilterSelect } = this.props;
+      const active = filter === name;
+      const activeClass = active ? "btn-info" : "btn-outline-secondary";
+      return (
+        <button
+          key={name}
+          color="info"
+          type="button"
+          className={`btn ${activeClass}`}
+          onClick={() => onFilterSelect(name)}
+        >
+          {label}
+        </button>
+      );
+    });
+    return <div className="btn-group">{buttons}</div>;
+  }
+}
