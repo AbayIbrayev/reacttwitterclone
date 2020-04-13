48a581bb0fa96e029b798990c162526072fe84db
Abay <abayibrayev7@gmail.com>
2020-04-13 11:46:55 +0100

    added a check for the post list items in the post-list

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -6,13 +6,22 @@ import "./post-list.css";
 
 const PostList = ({ posts, onDelete }) => {
   const elements = posts.map((post) => {
-    const { id, ...postProps } = post;
-    return (
-      <li key={id} className="list-group-item">
-        <PostListItem {...postProps} onDelete={() => onDelete(id)} />
-      </li>
-    );
+    if (typeof post === "object" && isEmpty(post)) {
+      const { id, ...postProps } = post;
+      return (
+        <li key={id} className="list-group-item">
+          <PostListItem {...postProps} onDelete={() => onDelete(id)} />
+        </li>
+      );
+    }
   });
+
+  function isEmpty(obj) {
+    for (let key in obj) {
+      return true;
+    }
+    return false;
+  }
 
   return <ListGroup className="app-list">{elements}</ListGroup>;
 };
